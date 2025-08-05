9bd57775c06c95b76294f8e269c7180f5733467b
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-08-05 14:54:15 +0530

    Forming Setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// src/app.js
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, // Allow cookies to be sent with requests
+}));
+app.use(cookieParser());
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended: true, limit:"16kb"}));
+app.use(express.static("public"));
+
+// Middleware, routes, etc.
+export {app};
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (reuestHandler) => {
+    (req, res, next) => {
+    Promise.resolve(reuestHandler(req, res, next)).catch((err)=> next(err))
+        
+}
+}
+export {asyncHandler}
+
+
+
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//        res.status(error.code||500).json({
+//         success: false,
+//         message: error.message || "Internal Server Error"
+//        }); 
+//     }
+// }
