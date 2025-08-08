65086fb1237821ccbd79e57893d10ad301666e2b
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-08-08 14:25:19 +0530

    complete setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -12,7 +12,7 @@ export {asyncHandler}
 //     try {
 //         await fn(req, res, next);
 //     } catch (error) {
-//        res.status(error.code||500).json({
+//        res.status(err.code||500).json({
 //         success: false,
 //         message: error.message || "Internal Server Error"
 //        }); 
