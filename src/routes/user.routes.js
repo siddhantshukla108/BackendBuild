7f0baedec9e8c566097e1bd613e8a7668356afcf
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-08-20 16:36:00 +0530

    add refresh token route to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -21,6 +21,7 @@ router.route("/register").post(
     router.route("/login").post(loginUser)
 
     router.route("/logout").post(verifyJWT, logoutUser)
+    router.route("/refresh-token").post(refreshAccessToken)
 
     
 
